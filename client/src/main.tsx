65afbcb8b7dd8b998af9de8402be3c82de88afec
hwrdyen <hwrdpvt@gmail.com>
2024-10-21 21:05:24 +0800

    Connected BE API endpoint with FE's store button to create snap-shot

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
+import { SnackbarProvider } from "notistack";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthContextProvider>
-      <App />
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
     </AuthContextProvider>
   </StrictMode>
 );
