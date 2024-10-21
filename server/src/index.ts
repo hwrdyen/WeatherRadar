4e157d467f6ed812b5a16337d3a8f3bb8f9901d6
hwrdyen <hwrdpvt@gmail.com>
2024-10-21 12:29:26 +0800

    Created open-meteo route and model, Setup layout and homepage

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import { Pool } from "pg";
 
+import openmeteoRoute from "../routes/openmeteo.route";
+
 const PORT = process.env.PORT || 5000;
 const connectionString = process.env.DATABASE_URL;
 
@@ -23,6 +25,8 @@ pool
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/openmeteo", openmeteoRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
